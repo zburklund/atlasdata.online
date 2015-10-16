a462703ff0638fb3877b01ab1110758f825e8468
Zach <zach.burklund@gmail.com>
2015-10-16 01:53:24 -0400

    color change

--- application/modules/template/views/two_col.php
+++ application/modules/template/views/two_col.php
@@ -1,7 +1,7 @@
 <html>
 	<head></head>
 	<body>
-		<div style="background-color:red; color:#ebebeb;">POOP ON MY HEAD 2 COL</div>
+		<div style="background-color:#803022;padding:50px;color:#ebebeb;">POOP ON MY HEAD 2 COL</div>
 
 		<?php
 		$this->load->view($module.'/'.$view_file);
